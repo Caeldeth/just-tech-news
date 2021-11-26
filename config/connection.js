dfce4aa56b7e8147ffb9e7898bae4df8757f4842
Caeldeth <sabrael@hybrasyl.com>
2021-11-26 14:40:50 -0600

    finish posts

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,5 @@
-//import the Sequilize constructor from the library
 const Sequelize = require('sequelize');
 require('dotenv').config();
-
 
 //create connection to our database, pass in your MySQL information for username and password
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
