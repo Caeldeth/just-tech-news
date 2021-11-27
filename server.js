73d70c9eda7450e5425b9fd4aaf06c5b77c1ed18
Caeldeth <sabrael@hybrasyl.com>
2021-11-27 17:15:17 -0600

    comments and upvotes

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const sequelize = require("./config/connection");
-const hbs = exphbs.create({});
-
-const session = require('express-session');
-
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: "Super secret secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
 };
 
 app.use(session(sess));
+
+const hbs = exphbs.create({});
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -31,6 +32,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(require("./controllers/"));
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log("Now listening"));
 });
