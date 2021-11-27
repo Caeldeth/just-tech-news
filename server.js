c7c59863e0a5023550e3dbed7522d830ed20e9bf
Caeldeth <sabrael@hybrasyl.com>
2021-11-27 14:48:38 -0600

    working login

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const sequelize = require("./config/connection");
-const { setDefaultResultOrder } = require("dns");
-
 const hbs = exphbs.create({});
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -21,5 +33,4 @@ app.use(require("./controllers/"));
 
 sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log("Now listening"));
-    seedAll();
 });
